e2563c61d13c948069a124a54ef2e6b4741e2cea
stban2703 <s.t.ban1401@gmail.com>
2020-03-31 00:08:05 -0500

    Se inicia el custom adapter

---
+++ app/src/main/java/com/example/practica9cliente/CustomAdapter.java
@@ -0,0 +1,4 @@
+package com.example.practica9cliente;
+
+public class CustomAdapter {
+}
--- app/src/main/java/com/example/practica9cliente/RegistroAdapter.java
+++ app/src/main/java/com/example/practica9cliente/RegistroAdapter.java
@@ -1,4 +1,28 @@
 package com.example.practica9cliente;
 
 public class RegistroAdapter {
+
+    private String nombre;
+    private String cedula;
+
+    public RegistroAdapter(String nombre, String cedula) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
 }

461139d51f5ed91a865404d3289f2a3faad662c2
stban2703 <s.t.ban1401@gmail.com>
2020-03-31 00:00:44 -0500

    Se inicia el adapter

--- app/src/main/java/com/example/practica9cliente/SummaryActivity.java
+++ app/src/main/java/com/example/practica9cliente/SummaryActivity.java
@@ -2,16 +2,20 @@ package com.example.practica9cliente;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class SummaryActivity extends AppCompatActivity implements ComunicacionTCP.OnMessageListener {
 
     private ComunicacionTCP comunicacionTcp;
+    private ListView listaRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
 
+        listaRegistro = findViewById(R.id.listaRegistro);
         comunicacionTcp = ComunicacionTCP.getInstance();
         comunicacionTcp.setObserver(this);
         comunicacionTcp.mandarMensaje("list");
@@ -19,6 +23,10 @@ public class SummaryActivity extends AppCompatActivity implements ComunicacionTC
 
     @Override
     public void onMessage(String mensaje) {
-
+        runOnUiThread(
+                () -> {
+                    Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+                }
+        );
     }
 }
